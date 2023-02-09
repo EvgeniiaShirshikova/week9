0a98f4350025f266ff8df593b4cf43adf7ca0e91
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-02-09 17:15:42 -0600

    created html,js files

---
+++ index.js
@@ -0,0 +1,30 @@
+let message = 'Hello!';
+let message2;
+message2 = 'world!';
+
+let x = 5;
+let y = x;
+x = 3;
+
+let n = 123;
+n = 12.3;
+
+//alert(n/message)
+
+let name = 'Jean';
+let name2 = "Jean";
+let name3 = `Jean`;
+
+let message_to = `Hello, ${name}`;
+let message_to2 = 'Hello, ' + name;
+
+//alert(message_to2)
+
+let yes = true;
+let no = false;
+
+let a = undefined;
+
+let b = null;
+
+console.log('hello world');
